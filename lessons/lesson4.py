545d99d3d6e4d9b9ff06a85fd34595bf03e63478
Maxim <11919911+Makasimka@users.noreply.github.com>
2024-09-02 10:31:25 +0500

    lesson 4

---
+++ lessons/lesson4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите любые данные: ')
+
+print(f'Длина строки: {len(my_string)}')
+print(f'В верхнем регистре: {my_string.upper()}')
+print(f'В нижнем регистре: {my_string.lower()}')
+print(f'Удалены пробелы: {my_string.replace(' ', '')}')
+print(f'Первый символ: {my_string[0]}')
+print(f'Последний символ: {my_string[-1]}')
