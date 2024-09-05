c29ae6661d9f5cb1741536d04715cfbdf3b54365
Maxim <11919911+Makasimka@users.noreply.github.com>
2024-09-05 11:44:48 +0500

    lesson 6

---
+++ lessons/lesson6.py
@@ -0,0 +1,35 @@
+my_dict = {
+	'Россия'  : 'Москва',
+	'Франция' : 'Париж',
+	'Латвия'  : 'Рига',
+	'Италия'  : 'Рим',
+	'Швеция'  : 'Стокгольм',
+	'Германия': 'Берлин',
+}
+
+print(f'Dict: {my_dict}')
+print(f'Existing value: {my_dict['Франция']}')
+print(f'Not existing value: {my_dict.get('Сербия')}')
+
+my_dict.update({
+	'Австрия': 'Вена',
+	'Бельгия': 'Брюссель',
+})
+print(f'Dict update: {my_dict}')
+
+delete_val = my_dict.pop('Германия')
+print(f'Deleted value:  {delete_val}')
+print(f'Modified dictionary: {my_dict}')
+
+
+my_set = { 0, 0, 0, 1, 1, 1, '1', '1', True, True, False, False, None, None, (1, 2), (3, 4), (1, 2), (3, 4), }
+# True, False - удаляются (заменяются) на 1 и 0, хотя на уроке про это не сказали.
+
+print(f'Set: {my_set}')
+
+my_set.add(2)
+my_set.add(3)
+print(f'Modified set: {my_set}')
+
+my_set.remove(0) # или .discard(), чтобы без ошибки
+print(f'Modified set (2): {my_set}')
